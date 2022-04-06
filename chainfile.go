6f1da31547b8cfb2f023daf38829de3b43f521e3
James Pirruccello <james@carbocation.com>
2022-04-06 11:05:21 -0400

    Convert the API to expect buffered readers as input, rather than filepaths.

--- chainfile.go
+++ chainfile.go
@@ -1,35 +1,33 @@
 package glo
 
 import (
-    "os"
-    "bufio"
-    "strings"
-    "fmt"
-    "net/http"
-    "compress/gzip"
+	"bufio"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
 )
 
-
 type ChainFile struct {
-    referenceBuild, queryBuild, filepath string
-    chains map[string][]*Chain
+	referenceBuild, queryBuild, filepath string
+	chains                               map[string][]*Chain
 }
 
 func (cf *ChainFile) add(chain *Chain) {
-    contig := chain.tName
-    cf.chains[contig] = append(cf.chains[contig], chain)
+	contig := chain.tName
+	cf.chains[contig] = append(cf.chains[contig], chain)
 }
 
-// fileExists checks if a file exists at the specified filepath, 
+// fileExists checks if a file exists at the specified filepath,
 // returning a corresponding boolean value.
 func fileExists(filepath string) bool {
-    file, err := os.Open(filepath)
-    file.Close()
-    if err == nil {
-        // File existed
-        return true
-    }
-    return false
+	file, err := os.Open(filepath)
+	file.Close()
+	if err == nil {
+		// File existed
+		return true
+	}
+	return false
 }
 
 // getFileType deterimines the MIME file type of the file located
@@ -37,121 +35,94 @@ func fileExists(filepath string) bool {
 // the file using http.DetectContentType(). The file type is returned
 // as a string.
 func getFileType(filepath string) string {
-    if !fileExists(filepath) {
-        fmt.Printf("Error: Specified file (%s) cannot be opened.\n", filepath)
-        os.Exit(1)
-    }
+	if !fileExists(filepath) {
+		fmt.Printf("Error: Specified file (%s) cannot be opened.\n", filepath)
+		os.Exit(1)
+	}
 
-    file, _ := os.Open(filepath)
-    defer file.Close()
+	file, _ := os.Open(filepath)
+	defer file.Close()
 
-    // Read the first 512 bytes from the file into the buffer. This should
-    // be sufficient to determine the filetype.
-    buffer := make([]byte, 512)
-    _, err := file.Read(buffer)
+	// Read the first 512 bytes from the file into the buffer. This should
+	// be sufficient to determine the filetype.
+	buffer := make([]byte, 512)
+	_, err := file.Read(buffer)
 
-    if err != nil {
-        fmt.Printf("Error opening file: %s\n", err)
-        os.Exit(1)
-    }
-    file.Close()
+	if err != nil {
+		fmt.Printf("Error opening file: %s\n", err)
+		os.Exit(1)
+	}
+	file.Close()
 
-    return string(http.DetectContentType(buffer))
+	return string(http.DetectContentType(buffer))
 }
 
 func get_chunk(reader *bufio.Reader, i int) string {
-    s := ""
-    p, p_err := reader.Peek(6)
-    if p_err == nil {
-        s = string(p)
-        pos := strings.Index(s, "\n")
-        if pos != -1 {
-            s = s[:pos]
-        }
-    }
-    return(string(s))
+	s := ""
+	p, p_err := reader.Peek(6)
+	if p_err == nil {
+		s = string(p)
+		pos := strings.Index(s, "\n")
+		if pos != -1 {
+			s = s[:pos]
+		}
+	}
+	return (string(s))
 }
 
-// ChainFile.Load loads the contents of the specified chain file
-// into the struct, storing them as a map of chains.
-func (cf *ChainFile) Load() {
-    cf.chains = make(map[string][]*Chain)
+// ChainFile.Load loads the contents of the specified reader into the struct,
+// storing them as a map of chains.
+func (cf *ChainFile) Load(reader *bufio.Reader) {
+	cf.chains = make(map[string][]*Chain)
 
-    // Determine if this is a plain text (uncompressed) chain file
-    // or a gzipped file. No other formats are supported at this time.
-    filetype := getFileType(cf.filepath)
-    if filetype[:11] == "text/plain;" {
-        filetype = "text"
-    } else if filetype == "application/x-gzip" {
-        filetype = "gzip"
-        //os.Exit(1)
-    } else {
-        fmt.Printf("Unsupported filetype: %s\n", filetype)
-        os.Exit(1)
-    }
+	// Use the reader to can through the entire file one
+	// line at a time, building the ChainFile structure full
+	// of Chain structs.
+	var chain *Chain
+	for {
+		// Peek ahead one byte to determine if the Reader can
+		// read any further data.
+		_, p_err := reader.Peek(1)
+		if p_err != nil {
+			// Peek encountered an error; assuming it is EOF
+			// so that the for look needs to terminate.
+			break
+		}
 
-    // getFileType already checks if the file exists, so we don't
-    // need to repeat that call here.
-    f, _ := os.Open(cf.filepath)
-    defer f.Close()
+		// Use helper function to peek at the up to next 6 bytes,
+		// returned as a string that contains anything before the
+		// newline character.
+		s := strings.TrimSpace(get_chunk(reader, 6))
 
-    // Conditionally either create a gzip or plain text reader
-    var reader *bufio.Reader
-    if filetype == "gzip" {
-        fgz, _ := gzip.NewReader(f)
-        reader = bufio.NewReader(fgz)
-    } else {
-        reader = bufio.NewReader(f)
-    }
+		if len(s) == 0 {
+			// Blank line, read until new line and discard.
+			reader.ReadString('\n')
+		} else {
+			if len(s) > 4 && s[:5] == "chain" {
+				// Start of a new chain.
+				line, _ := reader.ReadString('\n')
+				if chain != nil {
+					// Store this current chain before
+					// initializing a new one.
+					cf.add(chain)
+				}
+				chain = new(Chain)
+				chain.FromString(string(line))
+			} else if s[0] != '#' {
+				// This should be mapping block(s) that
+				// will get loaded into the current chain.
+				chain.load_links(reader)
+			} else {
+				// Junk line, should never hit this in a
+				// well-formatted chain file.
+				reader.ReadString('\n')
+			} // end inner if-elseif-else block
+		} // end outer if-else block
+	} // end for loop
 
-    // Use the reader to can through the entire file one
-    // line at a time, building the ChainFile structure full
-    // of Chain structs.
-    var chain *Chain
-    for {
-        // Peek ahead one byte to determine if the Reader can
-        // read any further data.
-        _, p_err := reader.Peek(1)
-        if p_err != nil {
-            // Peek encountered an error; assuming it is EOF
-            // so that the for look needs to terminate.
-            break
-        }
-
-        // Use helper function to peek at the up to next 6 bytes,
-        // returned as a string that contains anything before the
-        // newline character.
-        s := strings.TrimSpace(get_chunk(reader, 6))
-
-        if len(s) == 0 {
-            // Blank line, read until new line and discard.
-            reader.ReadString('\n')
-        } else {
-            if len(s) > 4 && s[:5] == "chain" {
-                // Start of a new chain.
-                line, _ := reader.ReadString('\n')
-                if chain != nil {
-                    // Store this current chain before
-                    // initializing a new one.
-                    cf.add(chain)
-                }
-                chain = new(Chain)
-                chain.FromString(string(line))
-            } else if s[0] != '#' {
-                // This should be mapping block(s) that
-                // will get loaded into the current chain.
-                chain.load_links(reader)
-            } else {
-                // Junk line, should never hit this in a
-                // well-formatted chain file.
-                reader.ReadString('\n')
-            } // end inner if-elseif-else block
-        } // end outer if-else block
-    } // end for loop
-
-    if chain != nil {
-        // Something unstored remains in a chain; store it.
-        cf.add(chain)
-        chain = nil
-    }
+	if chain != nil {
+		// Something unstored remains in a chain; store it.
+		cf.add(chain)
+		chain = nil
+	}
 }
--- liftover.go
+++ liftover.go
@@ -1,94 +1,91 @@
 package glo
 
 import (
-    "strconv"
+	"bufio"
+	"strconv"
+
+	"github.com/Workiva/go-datastructures/augmentedtree"
 )
 
-import "github.com/Workiva/go-datastructures/augmentedtree"
-
-
-
 type LiftOverTree struct {
-    reference, query string
-    contigs map[string]*augmentedtree.Tree
+	reference, query string
+	contigs          map[string]*augmentedtree.Tree
 }
 
 type LiftOverKey struct {
-    reference, query string
+	reference, query string
 }
 
 type LiftOver struct {
-    trees map[LiftOverKey]*LiftOverTree
+	trees map[LiftOverKey]*LiftOverTree
 }
 
 func (self *LiftOver) Init() {
-    self.trees = make(map[LiftOverKey]*LiftOverTree)
+	self.trees = make(map[LiftOverKey]*LiftOverTree)
 }
 
 func (self *LiftOver) Lift(reference_build, query_build string, query *ChainInterval) []*ChainInterval {
-    var overlaps []*ChainInterval
+	var overlaps []*ChainInterval
 
-    // Generate key for accessing the correct tree
-    key := new(LiftOverKey)
-    key.reference = reference_build
-    key.query = query_build
+	// Generate key for accessing the correct tree
+	key := new(LiftOverKey)
+	key.reference = reference_build
+	key.query = query_build
 
-    lotree, lotree_exists := self.trees[*key]
-    if lotree_exists {
-        atree, atree_exists := lotree.contigs[query.contig]
-        if atree_exists {
-            for _, res := range (*atree).Query(query) {
-                // Use type assertion to specify that the Interval
-                // being returned is a *ChainBlock, and a call to
-                // GetOverLap() to get the overlapped interval.
-                overlap := res.(*ChainLink).GetOverlap(query)
-                if (overlap.size() > 0) {
-                    overlaps = append(overlaps, overlap)
-                }
-            }
-        }
-    }
+	lotree, lotree_exists := self.trees[*key]
+	if lotree_exists {
+		atree, atree_exists := lotree.contigs[query.contig]
+		if atree_exists {
+			for _, res := range (*atree).Query(query) {
+				// Use type assertion to specify that the Interval
+				// being returned is a *ChainBlock, and a call to
+				// GetOverLap() to get the overlapped interval.
+				overlap := res.(*ChainLink).GetOverlap(query)
+				if overlap.size() > 0 {
+					overlaps = append(overlaps, overlap)
+				}
+			}
+		}
+	}
 
-    return overlaps
+	return overlaps
 }
 
-func (self *LiftOver) LoadChainFile(source, target, fp string) {
-    // Initialize new ChainFile object.
-    cf := new(ChainFile)
-    cf.referenceBuild = source
-    cf.queryBuild = target
-    cf.filepath = fp
-    // Load data
-    cf.Load()
+func (self *LiftOver) Load(source, target string, fp *bufio.Reader) {
+	// Initialize new ChainFile object.
+	cf := new(ChainFile)
+	cf.referenceBuild = source
+	cf.queryBuild = target
+	// Load data
+	cf.Load(fp)
 
-    // Generate key for tree mapping
-    key := new(LiftOverKey)
-    key.reference = source
-    key.query = target
+	// Generate key for tree mapping
+	key := new(LiftOverKey)
+	key.reference = source
+	key.query = target
 
-    tree := new(LiftOverTree)
-    tree.reference = source
-    tree.query = target
-    tree.contigs = make(map[string]*augmentedtree.Tree)
-    for contig, chains := range cf.chains {
-        _, exists := tree.contigs[contig]
-        if !exists {
-            t := augmentedtree.New(1)
-            tree.contigs[contig] = &t
-        }
-        atree := *(tree.contigs[contig])
-        for _, chain := range chains {
-            for _, link := range chain.links {
-                atree.Add(link)
-            }
-        }
-    }
+	tree := new(LiftOverTree)
+	tree.reference = source
+	tree.query = target
+	tree.contigs = make(map[string]*augmentedtree.Tree)
+	for contig, chains := range cf.chains {
+		_, exists := tree.contigs[contig]
+		if !exists {
+			t := augmentedtree.New(1)
+			tree.contigs[contig] = &t
+		}
+		atree := *(tree.contigs[contig])
+		for _, chain := range chains {
+			for _, link := range chain.links {
+				atree.Add(link)
+			}
+		}
+	}
 
-    self.trees[*key] = tree
+	self.trees[*key] = tree
 }
-
 
 func str2int64(s string) int64 {
-    val, _ := strconv.ParseInt(s, 10, 64)
-    return val
+	val, _ := strconv.ParseInt(s, 10, 64)
+	return val
 }
